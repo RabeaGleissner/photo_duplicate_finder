63855079a0d4ae224e48de7a16de98d214c0e80b
Rabea Gleissner <rabeagleissner@gmail.com>
2020-12-09 10:51:14 +0800

    Raise exception as guard clause

--- lib/args_parser.rb
+++ lib/args_parser.rb
@@ -8,12 +8,11 @@ class ArgsParser
     if arguments.length < 2
       return DEFAULT_DIRECTORY_NAME
     end
+
     directory_flag_index = arguments.find_index(DIRECTORY_FLAG)
 
-    if directory_flag_index
-      arguments[directory_flag_index + 1]
-    else
-      raise 'Invalid options given'
-    end
+    raise 'Invalid options given' unless directory_flag_index
+
+    arguments[directory_flag_index + 1]
   end
 end
